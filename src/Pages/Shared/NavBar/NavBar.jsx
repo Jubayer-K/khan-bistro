7592797a2e6d2e4bb08dad79ebe3e6c4d4e9b0a3
Jubayer-K <jubayerkhan21@gmail.com>
2024-05-19 04:19:35 +0600

    added testimonials section

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
   );
   return (
     <>
-      <div className="navbar fixed z-10 bg-transparent text-white">
+      <div className="navbar fixed z-10 bg-backdrop-blur-md bg-white/20 rounded-b-3xl">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
